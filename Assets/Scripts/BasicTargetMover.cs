1dd0ac678f9ff29ac7d2f7583374218f72ba0c01
AndyVen11 <62190225+AndyVen11@users.noreply.github.com>
2021-03-27 20:54:22 +0200

    Add files via upload

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+    // Update is called once per frame
+    void Update()
+    {
+        // Stops rotaion if desired
+        if(doSpin)
+        {
+            // Rotate around the y-axis in the game object
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+
+        // Stops motion if desired
+        if (doMotion)
+        {
+            // Move up and down over time
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+
+    }
+}
